3b30dac24f9ebd8a64d1a18613d36eff177c8f8f
CBickey <53806620+CBickey@users.noreply.github.com>
2020-10-29 13:27:44 +1100

    PersonB done

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*speed,0f , Input.GetAxis("Vertical")*Time.deltaTime*speed);
+    }
+}
