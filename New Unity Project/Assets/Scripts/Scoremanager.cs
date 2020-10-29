f90cdf67c3c0e0ff40008cbecf2cae0f72d59f07
CBickey <53806620+CBickey@users.noreply.github.com>
2020-10-29 14:07:49 +1100

    PersonA incomplete

---
+++ New Unity Project/Assets/Scripts/Scoremanager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoremanager : MonoBehaviour
+{
+    public int coinsGathered = 0;
+    public int score = 0;
+    public int coinsWorth = 20;
+    public int dummyvalue;
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (dummyvalue == 1/*player picks up coin*/)
+        {
+            coinsGathered++;
+        }
+
+        score = coinsGathered * coinsWorth;
+    }
+}
